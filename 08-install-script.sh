586f2e0c0de08d13e093ce68d2f762a4710b29d6
adityanimmaturi <adityanimmaturi1203@gmail.com>
2026-05-21 14:01:03 +0530

    Shell-script

--- 08-install-script.sh
+++ 08-install-script.sh
@@ -6,7 +6,7 @@ if [ $USERID -ne 0 ]; then
     exit 1
 fi
 
-dnf install mysqll -y
+dnf install mysql -y
 if [ $? -ne 0 ]; then
     echo "error: Failed to install MySQL."
     exit 1
@@ -20,4 +20,4 @@ if [ $? -ne 0 ]; then
     exit 1
 else
     echo "Git installed successfully."
-fi  
+fi   
